df83049f97888ee7e9283ed63d01c2122d668ec0
TanyaAng <t.j.angelova@gmail.com>
2022-08-27 22:56:49 +0300

    Exam preparation

--- GamesPlay/GamesPlay/app/models.py
+++ GamesPlay/GamesPlay/app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    email = models.EmailField()
+
+    age = models.IntegerField()
+
+    password = models.CharField(
+        max_length=30,
+    )
+
+    first_name = models.CharField(
+        max_length=30,
+    )
+
+    last_name = models.CharField(
+        max_length=30,
+        null=True,
+        blank=True,
+    )
+
+    profile_picture = models.URLField(
+        null=True,
+        blank=True,
+    )
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Game(models.Model):
+    title = models.CharField(
+        max_length=30,
+    )
+
+    category = models.CharField(
+        max_length=15,
+    )
+
+    rating = models.CharField()
+
+    max_level = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+
+    image_url = models.URLField()
+
+    summary = models.TextField(
+        null=True,
+        blank=True,
+    )
